3bbb0fc1db9de94baf9bb928822a1e518f67a306
ratoobi <69602231+ratoobi@users.noreply.github.com>
2022-05-21 22:47:39 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GithubFavorites from "./components/GithubFavorites";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <GithubFavorites />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/GithubFavorites.tsx
@@ -0,0 +1,3 @@
+export default function GithubFavorites() {
+  return <h1 className="text-4xl text-rose-500">hello</h1>;
+}
